5fcbf047280ed330e3f7bd7d8b4cc78690a9fe29
DipuBarua <you@example.com>
2023-09-01 23:06:28 +0600

    all categories elements are updating

--- JS/tube.js
+++ JS/tube.js
@@ -16,8 +16,54 @@ const displayCategories = (categories) => {
 
         const categoriesContainer = document.getElementById('categories-container');
         categoriesContainer.appendChild(categoriesBtn);
+        // console.log(categoriesBtn);
+
+        // category btn clicked 
+        categoriesBtn.addEventListener('click', () => {
+            // showElements();
+            // console.log('button clicked..');
+            loadAllElements(eachCategory);
+        })
     })
 }
+
+const loadAllElements = async (eachCategory) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${eachCategory.category_id}`);
+    const data = await res.json();
+    // console.log(data);
+    const elements = data.data;
+    console.log(elements);
+    displayAllElements(elements);
+
+}
+
+const displayAllElements = (elements) => {
+    elements.forEach(eachElement => {
+        const element = document.createElement('div');
+        element.classList = `card w-auto bg-base-100 shadow-xl`;
+        element.innerHTML = `
+    <figure><img src="${eachElement.thumbnail}" alt="thumbnail" /></figure>
+    <div class="flex">
+    <div class="bg-gray-500 "> 
+    <img class="rounded-full" src="${eachElement.authors[0]?.profile_picture}" alt="author" /> 
+    </div>
+    <div class="card-body">
+   
+      <h2 class="card-title">
+        ${eachElement.title}
+      </h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+      </div>
+    </div>
+    </div>
+    `;
+        const allElementsContainer = document.getElementById('all-elements-container');
+        allElementsContainer.appendChild(element);
+    })
+}
+
+
 
 
 loadTube();
