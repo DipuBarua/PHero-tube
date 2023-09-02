55018d53c7be81c4f5fd6ca9bc48a6752c7f7b16
DipuBarua <you@example.com>
2023-09-02 14:23:24 +0600

    empty data page updated with successfully searched

--- JS/tube.js
+++ JS/tube.js
@@ -1,7 +1,7 @@
 const loadTube = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
-    // console.log(data);
+    console.log(data);
     const categories = data.data;
     displayCategories(categories);
 
@@ -29,25 +29,44 @@ const loadAllElements = async (eachCategory) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${eachCategory.category_id}`);
     const data = await res.json();
     // console.log(data);
-    const elements = data.data;
-    // console.log(elements);
-    displayAllElements(elements);
 
+    const elements = data.data;
+    const message = data.message;
+    const empty = data.status;
+    // console.log(empty);
+    displayAllElements(elements, message, empty);
 }
 
-const displayAllElements = (elements) => {
+const displayAllElements = (elements, message, empty) => {
     // call the parent container
     const allElementsContainer = document.getElementById('all-elements-container');
+
     // clear the previous loaded all elements >>
     allElementsContainer.textContent = '';
 
+
+    // console.log(elements);
+    // console.log(message);
+    // console.log(empty);
+
     // empty container with message >> not found data......................
+    if (!empty) {
+        const emptyCatetory = document.createElement('div');
+        emptyCatetory.classList = `md:col-start-2`
+        emptyCatetory.innerHTML = `
+       <div class="flex flex-col justify-center"> 
+       <img src="Icon.png" alt="Empty">
+        <p class="text-3xl font-bold">${message}</p>
+        </div>
+        `;
+        allElementsContainer.appendChild(emptyCatetory);
+    }
 
-
-    elements.forEach(eachElement => {
-        const element = document.createElement('div');
-        element.classList = `card w-auto bg-base-100 shadow-xl`;
-        element.innerHTML = `
+    else {
+        elements.forEach(eachElement => {
+            const element = document.createElement('div');
+            element.classList = `card w-auto bg-base-100 shadow-xl`;
+            element.innerHTML = `
     <figure class="relative" >
     <img src="${eachElement.thumbnail}" alt="thumbnail"/>
     <div class="absolute right-0 bottom-0 text-blue-600 bg-black">${eachElement.others?.posted_date}</div>
@@ -71,9 +90,11 @@ const displayAllElements = (elements) => {
     </div>
     `;
 
-        // append the new element to the parent container 
-        allElementsContainer.appendChild(element);
-    })
+            // append the new element to the parent container 
+            allElementsContainer.appendChild(element);
+        })
+    }//>> else closed
+
 }
 
 
